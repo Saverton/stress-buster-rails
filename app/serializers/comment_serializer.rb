619de8e78e261965fc5e8afdf7a27d8c3205f5ca
Scott Meadows <Scottmeadows04@gmail.com>
2023-01-20 12:16:50 -0500

    finish user route

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -1,9 +1,13 @@
 class CommentSerializer < ActiveModel::Serializer
-  attributes :id, :body, :replies, :likes_count
+  attributes :id, :body, :replies, :likes_count, :liked_by_user
 
   belongs_to :user
 
   def likes_count
     object.likes.count
+  end
+
+  def liked_by_user
+    object.likes.where(user_id: sessions[:user_id]).any?
   end
 end
