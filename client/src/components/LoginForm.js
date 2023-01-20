21680381b0b7e17fbca5b3780d169fad3dead8a7
Daniel Clements <drclements@liberty.edu>
2023-01-19 20:47:47 -0500

    added Login wall with form and sign up

---
+++ client/src/components/LoginForm.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Button } from "../styled-components/Buttons";
+import Error  from "../styled-components/Error";
+import { Input } from "../styled-components/input";
+import { FormField } from "../styled-components/FormField";
+import { Label } from "../styled-components/Label";
+
+function LoginForm({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsLoading(true);
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }).then((r) => {
+      setIsLoading(false);
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <FormField>
+        <Label htmlFor="username">Username</Label>
+        <Input
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </FormField>
+      <FormField>
+        <Label htmlFor="password">Password</Label>
+        <Input
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </FormField>
+      <FormField>
+        <Button variant="fill" color="primary" type="submit">
+          {isLoading ? "Loading..." : "Login"}
+        </Button>
+      </FormField>
+      <FormField>
+        {errors.map((err) => (
+          <Error key={err}>{err}</Error>
+        ))}
+      </FormField>
+    </form>
+  );
+}
+
+export default LoginForm;
