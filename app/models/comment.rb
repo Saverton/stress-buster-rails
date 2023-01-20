6a6301b868cc4adfbc49a3824fdf1c5449f5abb0
Scott Meadows <Scottmeadows04@gmail.com>
2023-01-20 10:46:15 -0500

    add dependencies for destruction

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,9 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :user
 
-  has_many :likes
+  has_many :likes, dependent: :destroy
 
-  has_many :replies, class_name: 'Comment', foreign_key: 'parent_id'
+  has_many :replies, class_name: 'Comment', foreign_key: 'parent_id', dependent: :destroy
 
   belongs_to :parent, class_name: 'Comment', optional: true
 
