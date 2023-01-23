4c1b4a6481492274556bcb04634ca42c9f6b8e7f
Scott Meadows <Scottmeadows04@gmail.com>
2023-01-23 09:30:55 -0500

    rails deployment

--- bin/sb-build.sh
+++ bin/sb-build.sh
@@ -2,5 +2,9 @@
 # exit on error
 set -o errexit
 
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
 bundle install
 bundle exec rake db:migrate
